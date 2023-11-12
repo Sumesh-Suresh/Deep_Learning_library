df3d5c89e01c9b517ae24a88db4f24e257f77f50
Sumesh K S <115145152+Sumesh-Suresh@users.noreply.github.com>
2023-11-12 12:24:07 -0500

    Update resampling.py

--- mytorch/nn/resampling.py
+++ mytorch/nn/resampling.py
@@ -15,7 +15,7 @@ class Upsample1d():
         """
         self.A=A
 
-        Z = np.zeros((A.shape[0],A.shape[1],self.upsampling_factor*(A.shape[2]-1)+1),dtype='float64') # TODO
+        Z = np.zeros((A.shape[0],A.shape[1],self.upsampling_factor*(A.shape[2]-1)+1),dtype='float64') 
         for i in range(Z.shape[0]):
             k=0                         #future np.kron
             for j in Z[i]:
@@ -60,11 +60,8 @@ class Downsample1d():
             Z (np.array): (batch_size, in_channels, output_width)
         """
 
-        Z = np.zeros((A.shape[0],A.shape[1],int((A.shape[2]-1)/self.downsampling_factor+1)), dtype='float64') # TODO 
+        Z = np.zeros((A.shape[0],A.shape[1],int((A.shape[2]-1)/self.downsampling_factor+1)), dtype='float64') 
         
-        # NOTE  used own method here to implement the 3rd dimension of z. 
-        #  According to handout 3rd dimension =  A.shape[2]//self.downsampling_factor+1
-
         for i in range(A.shape[0]): # iteration batches
             k=0
             for j in A[i]:  # elements in each batches
@@ -86,12 +83,12 @@ class Downsample1d():
             dLdA (np.array): (batch_size, in_channels, input_width)
         """
 
-        dLdA = np.zeros((self.req_shap[0],self.req_shap[1],self.req_shap[2]),dtype='float64') # TODO
+        dLdA = np.zeros((self.req_shap[0],self.req_shap[1],self.req_shap[2]),dtype='float64') 
         for i in range(dLdZ.shape[0]):
             k=0
             for j in dLdA[i]:
                 j[::self.downsampling_factor]=dLdZ[i,k,:]
-                k+=1  # TODO
+                k+=1  
         
         # print('shape of dldz=',dLdZ.shape)
         # print('shape of dlda', dLdA.shape)
@@ -111,7 +108,7 @@ class Upsample2d():
             Z (np.array): (batch_size, in_channels, output_width, output_height)
         """
         self.A=A
-        Z = np.zeros((A.shape[0],A.shape[1],self.upsampling_factor*(A.shape[2]-1)+1,self.upsampling_factor*(A.shape[3]-1)+1),dtype='float64') # TODO
+        Z = np.zeros((A.shape[0],A.shape[1],self.upsampling_factor*(A.shape[2]-1)+1,self.upsampling_factor*(A.shape[3]-1)+1),dtype='float64') 
 
         for i in range(Z.shape[0]):          # iteration over batches
             for j in range(Z.shape[1]):      # iteration over channels
@@ -132,7 +129,7 @@ class Upsample2d():
             dLdA (np.array): (batch_size, in_channels, input_width, input_height)
         """
 
-        dLdA = np.zeros(self.A.shape,dtype='float64')  # TODO
+        dLdA = np.zeros(self.A.shape,dtype='float64')  
         for i in range(dLdZ.shape[0]):          # iteration over batches
             for j in range(dLdZ.shape[1]):      # iteration over channels
                 dLdA[i,j,:,:]=dLdZ[i,j,::self.upsampling_factor,::self.upsampling_factor]   # every alternate rows and columns of dldz matched to dldA       
@@ -153,7 +150,7 @@ class Downsample2d():
             Z (np.array): (batch_size, in_channels, output_width, output_height)
         """
 
-        Z = np.zeros((A.shape[0],A.shape[1],int((A.shape[2]-1)/self.downsampling_factor+1),int((A.shape[3]-1)/self.downsampling_factor+1)), dtype='float64')  # TODO
+        Z = np.zeros((A.shape[0],A.shape[1],int((A.shape[2]-1)/self.downsampling_factor+1),int((A.shape[3]-1)/self.downsampling_factor+1)), dtype='float64') 
         
         # NOTE  used own method here to implement the 3rd dimension of z. 
         #  According to handout 3rd dimension =  A.shape[2]//self.downsampling_factor+1 
@@ -175,8 +172,7 @@ class Downsample2d():
             dLdA (np.array): (batch_size, in_channels, input_width, input_height)
         """
 
-        dLdA = np.zeros((self.req_dim[0],self.req_dim[1],self.req_dim[2],self.req_dim[3]),dtype='float64')  # TODO
-
+        dLdA = np.zeros((self.req_dim[0],self.req_dim[1],self.req_dim[2],self.req_dim[3]),dtype='float64')  
         for i in range(dLdA.shape[0]):          # iteration over batches
             for j in range(dLdA.shape[1]):      # iteration over channels
                 dLdA[i,j,::self.downsampling_factor,::self.downsampling_factor]=dLdZ[i,j,:,:]    # every alternate rows and columns of Z matched to A
